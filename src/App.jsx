d362587396cd0db23030d00f5baefdc08889d083
Shivankit Agarwal <shivankit1210@gmail.com>
2024-07-31 20:12:24 +0530

    start

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Vendor from './assets/components/Vendor'
+
+function App() {
+
+  return (
+    <>
+      <Vendor/>
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/components/Additem.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Additem = () => {
+  return (
+    <div>Additem</div>
+  )
+}
+
+export default Additem
