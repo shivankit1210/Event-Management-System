414058f64c185f339453b2ab04b8ff11cf32a61f
Shivankit Agarwal <shivankit1210@gmail.com>
2024-08-01 08:09:29 +0530

    Editing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,34 @@
-import './App.css'
-import Vendor from './assets/components/Vendor'
+// import './App.css'
+// import Vendor from './assets/components/Vendor'
 
-function App() {
+// function App() {
 
+//   return (
+//     <>
+//       <Vendor/>
+//     </>
+//   )
+// }
+
+// export default App
+
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
+import Admin from './components/Admin/Admin';
+import Vendor from './components/Vendor/Vendor';
+import User from './components/User/User';
+
+const App = () => {
   return (
-    <>
-      <Vendor/>
-    </>
-  )
-}
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/admin/*" element={<Admin />} />
+      <Route path="/vendor/*" element={<Vendor />} />
+      <Route path="/user/*" element={<User />} />
+    </Routes>
+  );
+};
 
-export default App
+export default App;
+
